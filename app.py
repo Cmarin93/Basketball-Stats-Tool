633e4d0b06960538581eaa186269053d616d2c12
Carlos A. Marin <runsol64@gmail.com>
2019-05-10 20:06:23 -0700

    Initiated team menu selection screen.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from constants import TEAMS as teams
 import os
+
 
 def clear():
     os.system('clear' if os.name == 'posix' else 'cls')
 
 def invalid_option():
+    clear()
     print('Please enter a valid entery.\n')
     input('Press [Enter] to continue...')
 
@@ -21,13 +24,37 @@ def menu():
             print('\n')
             option = int(input('>>> '))
             if option == 1:
-                input('option 1 selected...')
-                #show teamstuff
+                team_menu()
             elif option == 2:
                 break
-        except:
-            input('please select a valid option.')
+            else:
+                invalid_option()
+        except ValueError:
+            invalid_option()
 
+def team_menu():
+    menu_looping = True
+    while menu_looping:
+        try:
+            clear()
+            print('\n')
+            print('Teams')
+            print('\n\n')
+            # make teams list into dictionary with numbers stored.
+            for team, num in zip(teams, '123'):
+                print("%s) %s" % (num, team))
+            print('\n')
+            choice = int(input('>>> '))
+            if choice == 1:
+                input('showing team1 stats')
+            elif choice == 2:
+                input('showing team2 stats')
+            elif choice == 3:
+                input('showing team3 stats')
+            else:
+                invalid_option()
+        except ValueError:
+            invalid_option()
 
 if __name__ == '__main__':
     print('running program...')
