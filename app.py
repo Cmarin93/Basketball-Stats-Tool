9ac33046943db3243edd8fa9b7eb51be7357d66e
Carlos A. Marin <runsol64@gmail.com>
2019-05-15 18:28:52 -0700

    progress

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from constants import TEAMS as teams
 import os
+from constants import PLAYERS
+from constants import TEAMS
 
 
 def clear():
     os.system('clear' if os.name == 'posix' else 'cls')
 
+def total_players():
+    total_players = 0
+    for player in PLAYERS:
+        total_players += 1
+    return total_players
+
+def total_teams():
+    total_teams = 0
+    for team in TEAMS:
+        total_teams += 1
+    return total_teams
+players_per_team = (total_players() / total_teams())
+input(players_per_team)
+# list of xp_player & in-xp_players
+def seperation():
+    experienced_players = []
+    inexperienced_players = []
+    for player in PLAYERS: #acessing each player dictionary
+        if player['experience'] == 'YES':
+            experienced_players.append(player)
+        elif player['experience'] == 'NO':
+            inexperienced_players.append(player)
+    return experienced_players, inexperienced_players
+
+experienced_players, inexperienced_players = seperation()
+input(len(experienced_players))
 def invalid_option():
     clear()
     print('Please enter a valid entery.\n')
@@ -33,16 +60,16 @@ def menu():
             invalid_option()
 
 def team_menu():
-    menu_looping = True
-    while menu_looping:
+    menu_looping1 = True
+    while menu_looping1:
         try:
             clear()
             print('\n')
             print('Teams')
             print('\n\n')
-            # make teams list into dictionary with numbers stored.
-            for team, num in zip(teams, '123'):
-                print("%s) %s" % (num, team))
+            # make teams dict. into a numbered list.
+            # for num, team in teams:
+            #     print("%s) %s" % (num, team))
             print('\n')
             choice = int(input('>>> '))
             if choice == 1:
@@ -57,5 +84,4 @@ def team_menu():
             invalid_option()
 
 if __name__ == '__main__':
-    print('running program...')
     menu()
