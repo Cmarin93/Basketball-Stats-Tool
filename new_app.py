c60b11e6fcb66fda9bfbc17df4ab1ae12d500158
Carlos A. Marin <45906528+Cmarin93@users.noreply.github.com>
2019-07-15 13:57:28 -0700

    forming clean data.

--- new_app.py
+++ new_app.py
@@ -1,20 +1,35 @@
-import constants
+import os
+from constants import PLAYERS, TEAMS
 
-print(constants)
-print(dir(constants))
-print(constants.PLAYERS) # [{'name' : 'Carlos Marin', 'exp' : 'Yes', etc...}]
-print(constants.TEAMS) # ['1', '2', '3']
-print(type(constants.PLAYERS)) # a list w/ dictionaries
-print(type(constants.TEAMS)) # a list.
-print('')
-print('x'*25)
-print('')
-print(constants.PLAYERS[0]) # dictionary of the 1st player.
-print(type(constants.PLAYERS[0])) # dictionary.
-print(dir(constants.PLAYERS[0]))
-print('')
-print(constants.PLAYERS[0].items())
-print(constants.PLAYERS[0].values())
-print(constants.PLAYERS[0].keys())
-print(constants.PLAYERS[0]['name'])
-print('')
+def clear():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+teams = {}
+for team in enumerate(TEAMS, 1): # each team is paired w/ a #
+    teams["team{0}".format(team[0])] = team[1]
+
+print(teams)
+# WORK TBD: Each team is it's own dictionary.
+
+
+
+    # TEAMS = {{team_number : 1, Name : Panthers, Players : a list of handled players}}
+
+
+# print(PLAYERS) # [{'name' : 'Carlos Marin', 'exp' : 'Yes', etc...}]
+# print(TEAMS) # ['1', '2', '3']
+# print(type(PLAYERS)) # a list w/ dictionaries
+# print(type(TEAMS)) # a list.
+# print('')
+# print('x'*25)
+# print('')
+# print(PLAYERS[0]) # dictionary of the 1st player.
+# print(type(PLAYERS[0])) # dictionary.
+# print(dir(PLAYERS[0]))
+# print('')
+# player1 = PLAYERS[0].items()
+# print(player1)
+# print(PLAYERS[0].values())
+# print(PLAYERS[0].keys())
+# print(PLAYERS[0]['name'])
+# print('')
