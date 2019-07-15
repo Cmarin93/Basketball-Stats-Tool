f28c4c6ca72130f2fd5e91868ca23b651149975c
Carlos A. Marin <45906528+Cmarin93@users.noreply.github.com>
2019-07-15 10:13:10 -0700

    scouting data transfer awareness

--- new_app.py
+++ new_app.py
@@ -2,7 +2,19 @@ import constants
 
 print(constants)
 print(dir(constants))
-print(constants.PLAYERS) #[{'name' : 'Carlos Marin', 'exp' : 'Yes', etc...}]
-print(constants.TEAMS) #['1', '2', '3']
-print(type(constants.PLAYERS))
-print(type(constants.TEAMS))
+print(constants.PLAYERS) # [{'name' : 'Carlos Marin', 'exp' : 'Yes', etc...}]
+print(constants.TEAMS) # ['1', '2', '3']
+print(type(constants.PLAYERS)) # a list w/ dictionaries
+print(type(constants.TEAMS)) # a list.
+print('')
+print('x'*25)
+print('')
+print(constants.PLAYERS[0]) # dictionary of the 1st player.
+print(type(constants.PLAYERS[0])) # dictionary.
+print(dir(constants.PLAYERS[0]))
+print('')
+print(constants.PLAYERS[0].items())
+print(constants.PLAYERS[0].values())
+print(constants.PLAYERS[0].keys())
+print(constants.PLAYERS[0]['name'])
+print('')
