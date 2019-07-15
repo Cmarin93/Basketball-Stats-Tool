aa31e1db34f3056604f215c312e32156c721b618
Carlos A. Marin <45906528+Cmarin93@users.noreply.github.com>
2019-07-15 09:32:37 -0700

    Attempt #2
    Extracting and Forming the constants data

---
+++ new_app.py
@@ -0,0 +1,8 @@
+import constants
+
+print(constants)
+print(dir(constants))
+print(constants.PLAYERS) #[{'name' : 'Carlos Marin', 'exp' : 'Yes', etc...}]
+print(constants.TEAMS) #['1', '2', '3']
+print(type(constants.PLAYERS))
+print(type(constants.TEAMS))
