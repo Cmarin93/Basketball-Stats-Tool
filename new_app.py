f016f1965f597a71db32f54a0e8d15629a46d97e
Carlos A. Marin <45906528+Cmarin93@users.noreply.github.com>
2019-07-17 08:34:07 -0700

    created 2 experience-based list.

--- new_app.py
+++ new_app.py
@@ -5,6 +5,8 @@ def clear():
     os.system('clear' if os.name == 'posix' else 'cls')
 
 teams = []
+experienced_players = []
+inexperienced_players = []
 
 #for loop to create filtered data.
 for player in PLAYERS:
@@ -20,45 +22,27 @@ for player in PLAYERS:
         if player['guardians'].find(' and ') == -1:
             guard.append(player['guardians'])
         else:
-            pass
             dog = player['guardians']
             guard.append(dog[:player['guardians'].find(' and ')])
             guard.append(dog[player['guardians'].find(' and ') + 5:])
+            player['guardians'] = guard
 
+        if player['experience'] == 'YES':
+            player['experience'] = True
+            experienced_players.append(player)
+        else:
+            player['experience'] = False
+            inexperienced_players.append(player)
 
-    #print(player['guardians'])
-    #print(gaurd)
-    # print(type(player['guardians']))
-    #slice eeach character, if character is ' and ', provide action.
-
-        # select easch gaurdian and put them into own string.
-        #1) we can look for 2nd space. if not present, put into list.\
-        #    if present, remove " and " then place both ends into the list as seperate values.
-
-# pairing teams w/ #'s.
+# creation of team dictionaries
 for team_tuple in enumerate(TEAMS, 1): # each team is paired w/ a #
     team = {}
     team['name'] = team_tuple[1]
     team['number'] = team_tuple[0]
     teams.append(team)
 
-experienced_players = []
-inexperienced_players = []
-
-#1st change all players height.
-
-print(teams)
-print(teams[0])
-
-
-
-#I must replicate the PLAYERS (a list of dictionaries)
-# I must access every PLAYERS['height'] & convert to int.
-# I must access PLAYERS['experience'] & convert to bool.
-# I must acess PLAYERS['gaurdians'] & convert to a list of strings (remove 'and')
-
-# WORK TBD: {Players : [{Alex}, {Carlos}, {Dan}]}
-#           Must clean Player Data.
+print(experienced_players)
+print(inexperienced_players)
 
 
 
