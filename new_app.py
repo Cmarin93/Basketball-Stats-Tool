41cb622218e067564c9d0470e8996f711bf571c4
Carlos A. Marin <45906528+Cmarin93@users.noreply.github.com>
2019-07-17 13:27:06 -0700

    refinement

--- new_app.py
+++ new_app.py
@@ -13,32 +13,31 @@ teams_list = []
 def start_program():
     # Data restructuring
     for player in players:
-        client = player
-        height = client['height']
-        guardians_slice_index = client['guardians'].find(' and ')
+        height = player['height']
+        guardians_slice_index = player['guardians'].find(' and ')
 
         # slices string of 'height' then converts to int.
         if type(height) != int:
-            client['height'] = int(height[0:2])
+            player['height'] = int(height[0:2])
 
         # converts each string of 'guardians' into a list of strings.
-        if type(client['guardians']) == str:
+        if type(player['guardians']) == str:
             guardians = []
             if guardians_slice_index == -1:
-                guardians.append(client['guardians'])
-                client['guardians'] = guardians
+                guardians.append(player['guardians'])
+                player['guardians'] = guardians
             else:
-                dog = client['guardians']
-                guardians.append(dog[:guardians_slice_index])
-                guardians.append(dog[guardians_slice_index + 5:])
-                client['guardians'] = guardians
+                guardian_string = player['guardians']
+                guardians.append(guardian_string[:guardians_slice_index])
+                guardians.append(guardian_string[guardians_slice_index + 5:])
+                player['guardians'] = guardians
             # sorted players list based on experience.
-            if client['experience'] == 'YES':
-                client['experience'] = True
-                experienced_players.append(client)
+            if player['experience'] == 'YES':
+                player['experience'] = True
+                experienced_players.append(player)
             else:
-                client['experience'] = False
-                inexperienced_players.append(client)
+                player['experience'] = False
+                inexperienced_players.append(player)
     # creation of team dictionaries
     for team_tuple in enumerate(teams, 1):  # each team is paired w/ a #
         team = {}
