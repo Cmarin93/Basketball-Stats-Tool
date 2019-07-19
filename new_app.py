772c9e3df8467a051e563db87ff40aeb64c53bde
Carlos A. Marin <45906528+Cmarin93@users.noreply.github.com>
2019-07-19 06:18:53 -0700

    Update!!!!!

--- new_app.py
+++ new_app.py
@@ -11,6 +11,17 @@ team_list = []
 
 
 def start_program():
+
+    # creation of team dictionaries
+    for team_tuple in enumerate(teams, 1):  # each team is paired w/ a #
+        team = {}
+        team['team_name'] = team_tuple[1]
+        team['team_number'] = team_tuple[0]
+        team['team_total'] = int(len(players) / len(teams))
+        team['exp_registar'] = []
+        team['inexp_registar'] = []
+        team_list.append(team)
+
     # Data restructuring
     for player in players:
         height = player['height']
@@ -23,7 +34,7 @@ def start_program():
         # converts each string of 'guardians' into a list of strings.
         if type(player['guardians']) == str:
             guardians = []
-            if guardians_slice_index == -1:
+            if guardians_slice_index == -1: # if no ' and ' is found.
                 guardians.append(player['guardians'])
                 player['guardians'] = guardians
             else:
@@ -35,51 +46,29 @@ def start_program():
             if player['experience'] == 'YES':
                 player['experience'] = True
                 team_assign(player)
-                #lopp thru teams, and if exp slot is open, insert
             else:
-                player['experience'] = False 
+                player['experience'] = False
                 team_assign(player)
-    # creation of team dictionaries
-    for team_tuple in enumerate(teams, 1):  # each team is paired w/ a #
-        team = {}
-        team['team_name'] = team_tuple[1]
-        team['team_number'] = team_tuple[0]
-        team['team_total'] = int(len(players) / len(teams))
-        #should this be derived from len(exp_players)?
-        team['team_exp_total_limit'] = int(len(experienced_players) / len(teams))
-        team['team_inexp_total_limit'] = int(len(inexperienced_players) / len(teams))
-        team['exp_registar'] = []
-        team['inexp_registar'] = []
 
-        team_list.append(team)
-    print(team_list[0])
-    print(len(team['exp_registar']))
-    print(experienced_players)
 
+# balances players based on experience
 def team_assign(player):
     if player['experience'] == True:
         experienced_players.append(player)
-        print('HOTDOGHOTDOG')
+        for team in team_list:
+            if len(team['exp_registar']) < 3:
+                team['exp_registar'].append(player)
+                continue
+            else:
+                pass
     else:
         inexperienced_players.append(player)
-        print('WTF')
-    #add player to a team with less then
-
-    #team_list - everytime we loop thru a player.
-            #team_name \/
-            #team_number \/
-            #team_total:\/
-                #  total_players / #_of_teams = players_per_team
-            #team_exp
-                #  total_exp_players / #_of_teams = XP_players_per_group
-            #team_inexp
-                # total_exp_players / #_of_teams = XP_players_per_group
-            #team_players
-                # we must loop thru each team & assign them a xpPlayer until all xpPlayers are done?
-
-        #BALANCE: same # of total players on each team.
-        #BALANCE_XP: same # of experienced vs inexperienced_players
-
+        for team in team_list:
+            if len(team['inexp_registar']) < 3:
+                team['inexp_registar'].append(player)
+                continue
+            else:
+                pass
 
 
 def clear():
